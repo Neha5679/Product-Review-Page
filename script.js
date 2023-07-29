62fa3480830b29a84050e92789cf1890d577935f
Neha <129548970+Neha5679@users.noreply.github.com>
2023-07-29 21:29:43 +0530

    Add files via upload
    Its a product review page using html, css and javascript

---
+++ script.js
@@ -0,0 +1,24 @@
+var searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>
+{
+    searchForm.classList.toggle('active');
+    
+    navbar.classList.remove('active');
+}
+
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>
+{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>
+
+{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+}
